16b1262101eb6643f7f8c56c074fff77422bede4
Esha Pednekar <egpednekar@gmail.com>
2025-02-24 12:28:21 -0500

    global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,27 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #0f0f0f;
+    color: white;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+  },
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
